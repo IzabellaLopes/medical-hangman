2c5c7f68238f26d3c6d5253d25d0689f4a9ef212
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-08-13 03:32:37 +0100

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('medical_hangman')
+WORD_SHEET = SHEET.worksheet('word_list')
+
+bones_words = WORD_SHEET.col_values(1)
+organs_words = WORD_SHEET.col_values(2)
+diseases_words = WORD_SHEET.col_values(3)
+radiology_words = WORD_SHEET.col_values(4)
+
+print(bones_words)
+print(organs_words)
+print(diseases_words)
+print(radiology_words)
+
+
