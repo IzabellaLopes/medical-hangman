b89c0c92464cf415c647bceee06c24a9d4d3fda6
Izabella Lopes <bellaccarvalho@yahoo.com.br>
2023-08-14 02:19:20 +0100

    Add 7 attempts limit for guessing the word

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # Open the 'medical_hangman' Google Sheets
 SHEET = GSPREAD_CLIENT.open('medical_hangman')
 
+# Function to select a random word from a specified column in a Google Sheets worksheet
 def select_random_word_from_sheet(sheet, column_number):
     """
     Selects a random word from a specified column in a Google Sheets worksheet.
@@ -36,13 +37,15 @@ def select_random_word_from_sheet(sheet, column_number):
     random_word = random.choice(words)
     return random_word
 
+# Function to create a hidden word with underscores
 def create_hidden_word(word):
     """
     Creates a string of underscores to match the length of the input word.
     """
-    hidden_word = ' '.join(['_' if char != ' ' else ' ' for char in word])
+    hidden_word = ' '.join([' _ ' if char != ' ' else ' ' for char in word])
     return hidden_word
 
+# Function to take a validated guess input from the user
 def take_guess():
     """
     Takes a validated guess input from the user. 
@@ -55,6 +58,7 @@ def take_guess():
         else:
             return guess
         
+# Function to check if the guessed letter is in the word and update the hidden word
 def check_guess(word, hidden_word, guess):
     """
     Checks if the guessed letter is in the word and updates the hidden word accordingly.
@@ -71,7 +75,7 @@ def check_guess(word, hidden_word, guess):
     
     return updated_hidden_word, correct_guess
 
-# Game Menu
+# Game Menu and main function
 def main():
     """
     Main function to run the Medical Hangman game with different word categories.
@@ -116,8 +120,9 @@ def main():
                     hidden_word = create_hidden_word(selected_word)
 
                     print(f"Hidden {category_name} word:", hidden_word)
-
-                    while '_' in hidden_word:
+                    
+                    attempts = 7  # Maximum number of attempts
+                    while '_' in hidden_word and attempts > 0:
                         guess = take_guess()
                         hidden_word, correct_guess = check_guess(selected_word, hidden_word, guess)
 
@@ -125,10 +130,15 @@ def main():
                             print("Correct guess!")
                         else:
                             print("Incorrect guess!")
+                            attempts -= 1
+                            print(f"{attempts} attempts left.")
 
                         print(f"Hidden {category_name} word:", hidden_word)
 
-                    print(f"Congratulations, you've guessed the {category_name} word!")
+                    if '_' in hidden_word:
+                        print("Sorry, you've run out of attempts. The word was:", selected_word)
+                    else:
+                        print(f"Congratulations, you've guessed the {category_name} word!")
                     break
                 else:
                     print("Invalid category choice. Please select a valid option.")
@@ -138,6 +148,6 @@ def main():
             print("Highscores: ...")   # Add highscores
         else:
             print("Invalid option. Please select a valid option.")
-
+        
 if __name__ == "__main__":
     main()
